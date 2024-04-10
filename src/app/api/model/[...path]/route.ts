42adaa7d07a6a5493c61ae4e6cfeb4c5abaf6134
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-04-10 17:17:57 +0800

    update

--- src/app/api/model/[...path]/route.ts
+++ src/app/api/model/[...path]/route.ts
@@ -23,5 +23,5 @@ const handler = NextRequestHandler({ getPrisma, useAppDir: true });
 
 export { handler as DELETE, handler as GET, handler as PATCH, handler as POST, handler as PUT };
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 export const dynamic = 'force-dynamic';

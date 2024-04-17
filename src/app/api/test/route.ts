cf62384fda01e00c4426983202dfd58365952b10
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-04-17 14:53:32 +0800

    update

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { createPrisma } from '~/server/db';
-
-export const dynamic = 'force-dynamic';
-export const runtime = 'edge';
-
-export async function GET(_request: Request) {
-    const prisma = createPrisma();
-    return Response.json(await prisma.list.findMany());
-}
